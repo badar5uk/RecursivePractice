7647a7fd6a87f724fbd71fc2eea4726b1242d449
bader <bader5uk@hotmail.com>
2024-10-27 10:58:15 +0400

    counts number if blocks in a triangle

---
+++ src/TriangleBlockCount.java
@@ -0,0 +1,27 @@
+public class TriangleBlockCount {
+    /*
+    Input: Number of rows in a triangle
+    Output: number of blocks in the triangle
+    Process: - using recursive method, the triangle is split into blocks
+             - each row will contain a smaller amount of blocks than the previous
+             - if the triangle 10 rows, it subtracts it and adds the number each time
+             - use a recursive function to check n
+             - if n is greater than 0 then it adds the number to the function and subtracts 1
+             - blocks = n + triangle( n-1)
+
+     */
+
+    public static void main(String[]args){
+        System.out.println(triangle(2));
+
+    }
+    public static Integer triangle(Integer n){
+        Integer sum =0;
+        if(n == 0){
+            return 0;
+        }else {
+            sum = n + triangle(n-1);
+        }
+        return sum;
+    }
+}

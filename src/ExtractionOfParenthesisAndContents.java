6d0e6d76fd73db50815e6ff80007de2b202aeda7
bader <bader5uk@hotmail.com>
2024-10-28 08:48:49 +0400

    removes characters before parentheses, returns only the string inside the parenthesis

---
+++ src/ExtractionOfParenthesisAndContents.java
@@ -0,0 +1,21 @@
+public class ExtractionOfParenthesisAndContents {
+
+    public static void main(String[] args) {
+        System.out.println(parenBit("jdfj(Hello) hdghf"));
+
+    }
+
+    public static String parenBit(String str) {
+
+        if (str.charAt(0) != '(' ) {
+            str = parenBit(str.substring(1));
+            return str;
+        } else if(str.charAt(str.length() - 1) != ')') {
+            str = parenBit(str.substring(0,str.length()-1));
+            return str;
+        } else{
+            return str;
+        }
+    }
+}
+

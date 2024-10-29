49a08862395a3c7339cdfe7a2f9548da354dac30
bader <bader5uk@hotmail.com>
2024-10-29 11:31:32 +0400

    splits input array into two sub arrays and checks if the elements of the two arrays sum up

---
+++ src/ArraySplitWithConstraints.java
@@ -0,0 +1,36 @@
+
+import java.util.List;
+
+public class ArraySplitWithConstraints {
+
+    static List<Integer> nums = List.of(2,3,5);
+
+    public static void main(String[] args) {
+        System.out.println(split53(nums));
+
+    }
+
+    public static Boolean split53(List<Integer> nums) {
+        Integer sum = 0;
+        for (Integer number : nums) {
+            sum += number;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        Integer target = sum / 2;
+        return helperFunctionSplit(nums, 0, target);
+    }
+
+    public static Boolean helperFunctionSplit(List<Integer> nums, Integer index, Integer target) {
+        if (target == 0) {
+            return true;
+        } else if (index >= nums.size()) {
+            return false;
+        } else if (helperFunctionSplit(nums,index + 1, target - nums.get(index))){
+            return true;
+        }
+        return helperFunctionSplit(nums, index + 1, target);
+    }
+}
+

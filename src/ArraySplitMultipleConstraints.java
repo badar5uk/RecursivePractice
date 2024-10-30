c1a39d2b416dfc4f0014263dd643e74d965eefdc
bader <bader5uk@hotmail.com>
2024-10-30 10:03:09 +0400

    added comments

--- src/ArraySplitMultipleConstraints.java
+++ src/ArraySplitMultipleConstraints.java
@@ -3,6 +3,23 @@ import java.util.List;
 
 public class ArraySplitMultipleConstraints {
 
+    /*
+    Input: An array of integers taken as a list.
+    Output: a Boolean to specify if the integers can be split into two arrays
+            with their sum being equal to each other, this would also check if the two
+            sub arrays contain multiples 5 and 3.
+    Process: - Take an array of integers a global
+             - introduce 2 functions, a helper and the split function
+             - the split function would first check if the sum of the numbers is even or odd
+               this would check if they can be split equally.
+
+             -The helper function would then be accessed by the split53 function
+             - the numbers would be checked if are multiples of 5 or 3
+             - they would be separated into two different arrays by checking if the modulus of the
+                number equals 0.
+
+     */
+
     static List<Integer> nums = List.of(6, 9, 10, 5);
 
     public static void main(String[] args) {

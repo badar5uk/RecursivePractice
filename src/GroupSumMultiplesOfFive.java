1c4178c4a87238e900016b9d373497994389baf6
bader <bader5uk@hotmail.com>
2024-10-30 11:38:09 +0400

    Takes elements and checks if multiples of 5 are equal to the target, if not it looks for another element to add

---
+++ src/GroupSumMultiplesOfFive.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupSumMultiplesOfFive {
+    /*
+        Input: An array of integers taken as a list and a target sum to be reached
+        Output: a Boolean to specify if target sum can be met with the numbers
+         and if there is a multiple of five.
+        Process: - Take an array of integers and the target sum as global
+                 - introduce 2 functions, a helper and groupSum5
+                 - the sum5 function would split multiples of 5 by themselves
+                 - a sum pf those elements would be checked if they meet target
+                 - if they don't then it accesses the helper function
+                 - the helper function would then take input of the sum and adjusted list
+                 - it would iterate to get to the target, once it is met it returns true
+
+                */
+    static List<Integer> nums = new ArrayList<>(Arrays.asList(2, 5, 10, 4));
+    static Integer target = 19;
+
+    static List<Integer> tempListMultiples = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println(helperSumChecker());
+
+    }
+
+    public static Boolean helperSumChecker() {
+        Integer sumFive = 0;
+
+        for (Integer num : nums) {
+            if (num % 5 == 0) {
+                tempListMultiples.add(num);
+                sumFive += num;
+            }
+        }
+
+        if (sumFive == target) {
+            return true;
+        } else {
+            for (Integer tempNum : tempListMultiples) {
+                nums.remove(tempNum);
+            }
+            return groupSum5(0, nums, target, sumFive);
+        }
+
+    }
+
+
+    public static Boolean groupSum5(Integer index, List<Integer> nums, Integer target, Integer sumFive) {
+
+        if (sumFive + nums.get(index) == target) {
+            return true;
+        } else if (index == nums.size() - 1) {
+            return false;
+        } else {
+            return groupSum5(index + 1, nums, target, sumFive);
+        }
+    }
+}
+

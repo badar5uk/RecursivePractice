f9cb8eeb15665ed0b50be5e2db36b60911e4c73c
bader <bader5uk@hotmail.com>
2024-10-30 09:56:15 +0400

    added comments

--- src/ArraySplitEqualSum.java
+++ src/ArraySplitEqualSum.java
@@ -2,6 +2,21 @@
 import java.util.List;
 
 public class ArraySplitEqualSum {
+    /*
+    Input: An array of integers taken as a list.
+    Output: a Boolean to specify if the integers can be split into twwo arrays
+            with their sum being equal to each other
+    Process: - Take an array of integers a global
+             - introduce 2 functions, a helper and the split function
+             - the split function would first check if the sum of the numbers is even or odd
+               this would check if they can be split equally.
+
+             -The helper function would then be accessed by the split function
+             - the function would increment the index and go through the list while subtracting
+               from the target sum each time until we reach zero
+             - if the index reaches the end of the list it and the sum subtraction isnt met
+                it would return false
+     */
 
     static List<Integer> nums = List.of(2,3,5);
 
